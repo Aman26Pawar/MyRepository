3a91cf128314b4b75137ca79b44f7108c6974bfa
Aman26Pawar <Amanjpawar.1997@gmail.com>
2018-08-08 19:54:09 +0530

    MVC added 1

--- MoviesMVC/MoviesMVC/Controllers/MoviesController.cs
+++ MoviesMVC/MoviesMVC/Controllers/MoviesController.cs
@@ -14,6 +14,7 @@ namespace MoviesMVC.Controllers
         // GET: Movies
         public ActionResult Index()
         {
+            var movieInfo = movieDB.MovieInfoes.Include(m => m.MovieInfo);
             return View();
         }
 
