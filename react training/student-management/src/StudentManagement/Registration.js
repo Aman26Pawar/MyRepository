a55515d55e903c6deefe9ded1c761c12d85becbc
Aman26Pawar <Amanjpawar.1997@gmail.com>
2018-09-04 16:03:07 +0530

    validate functions added

--- react training/student-management/src/StudentManagement/Registration.js
+++ react training/student-management/src/StudentManagement/Registration.js
@@ -12,99 +12,130 @@ export default class Registration extends Component{
             errors:{}
         }
     }
-    onSubmitClick = () => {
-        //let fields = this.state.fields;
-        let errors = {};
+
+    validateFirstName = (First_name) =>{
+        //let errors = {};
         let formIsValid = false;
         let msg = "";
-        //let nameField = document.getElementById('name');
-
-
-        //Name
-        /*if(!fields["name"]){
-           formIsValid = false;
-           errors["name"] = "Cannot be empty";
-        }*/
-        //this.validateBaiscChecks(nameField);
-       var isRequired = document.getElementById('name').getAttribute('required') === "true";
-
-        const First_name = document.getElementById("name").value;
+       var isRequired = document.getElementById('name').required    
         if(isRequired && First_name !== ""){
-            if(First_name.size() > 30) {
-                msg = "Only Letters allowed";
-            }
-            else if(First_name.match(/^[a-zA-Z]+$/)){
+            if(First_name.length > 10) {
+                msg = "length should not exceed 10 letters";
+            }else if(First_name.match(/^[a-zA-Z]+$/)){
                 formIsValid = true;
                 this.setState({FirstName : First_name});
-            } else {
+            }else {
                 msg = "Only Letters allowed"
             }
          }
-
-         var errorElement = document.getElementsByClassName('.error.name');
-         if(!formIsValid) {
-            errorElement.removeClass('hide');
-            document.getElementsByClassName('.error.name').value = msg;
-            //find how to add remove classes in javascript.
-            // addClass., removeClass.
+         //let errorMsg = msg;
+         var errorElement = document.getElementsByClassName('error');
+         if(formIsValid === true) {
+            //errorElement.className =errorElement.className.removeClass('hide')
+            //document.getElementsByClassName('error').value = msg;
+            return formIsValid;
          } else {
             errorElement.className += 'hide';
          }
-
-        const last_name = document.getElementById("lastName").value;
-        if(last_name !== ""){
-            if(last_name.match(/^[a-zA-Z]+$/)){
-                this.setState({LastName : last_name})
-            }  
-            else{
-                formIsValid = false;
-               // errors[last_name] = "Only letters";
-               
+        
+    }
+    validateLastName = (Last_name) =>{
+        //let errors = {};
+        let formIsValid = false;
+        let msg = "";
+       var isRequired = document.getElementById('lastName').required    
+        if(isRequired && Last_name !== ""){
+            if(Last_name.length > 10) {
+                msg = "length should not exceed 10 letters";
+            }else if(Last_name.match(/^[a-zA-Z]+$/)){
+                formIsValid = true;
+                this.setState({LastName : Last_name});
+            }else {
+                msg = "Only Letters allowed"
             }
          }
-       
-        const userNm = document.getElementById("userName").value;
-        this.setState({userNm:userNm });
-        const passWord = document.getElementById("password").value;
-        if(passWord.size < 6){
-            errors[passWord] = "password must be at least 6 digits";
-            alert(passWord.size())
-        }
-       
-        this.setState({passWord:passWord });
-        alert(`${this.state.FirstName} ${last_name}, registered succesfully`)
-
-        this.setState({errors: errors});
-        return formIsValid;
+        // let errorMsg = msg;
+         //var errorElement = document.getElementsByClassName('error');
+         if(formIsValid === true) {
+            //errorElement.className =errorElement.className.removeClass('hide')
+            //document.getElementsByClassName('error').value = msg;
+            return formIsValid;
+         } else {
+            //errorElement.className += 'hide';
+         }
         
-        /*if(First_name ==="" || last_name==="" || userNm===""||passWord===""){
-            alert("please fill all he information")
+    }
+    validatePassword = (passWord) =>{
+        let formIsValid = false;
+        let msg=""
+        var isRequired = document.getElementById('pass_word').required 
+        if(isRequired && passWord !==""){
+            if(passWord.length < 6){
+                msg = "password must contain atleast 6 digits"
+            }else{
+                formIsValid = true
+                this.setState({passWord:passWord });
+            } 
         }
-        else{
-            alert(`${First_name} ${last_name}, registered succesfully`)
-        }*/
 
-    }
-    handlePassword(){
+        if(formIsValid === true){
+            return formIsValid
+        }else{
 
+        }
     }
-   /* contactSubmit(e){
-        alert("submit")
+    validateUserName = (user_Name) =>{
+        let formIsValid = false;
+        let msg 
+        var isRequired = document.getElementById('user').required 
+
+        if(isRequired && user_Name!==""){
+            if(user_Name.match(/^[a-z0-9]+$/)){
+                formIsValid = true
+                this.setState({userNm:user_Name });
+                return formIsValid
+            }else{
+                msg = "user name should be alphanumeric"
+            }
+        }
     }
-    handleChange(field,e){
-        let fields = this.state.fields;
-        fields[field] = e.target.value;        
-        this.setState({fields});
-    }*/
+    onSubmitClick = () => {
+        //let fields = this.state.fields;
+       // let errors = {};
+        //let formIsValid = false;
+        //let msg = "";
+        //let nameField = document.getElementById('name');
+
+         //Name
+        const First_name = document.getElementById("name").value;
+        const last_name = document.getElementById("lastName").value;
+        const userNm = document.getElementById("user").value;
+        const passWord = document.getElementById("pass_word").value;
+        //this.validateFirstName(First_name);
+        if(this.validateFirstName(First_name) && this.validateLastName(last_name) && this.validateUserName(userNm) && this.validatePassword(passWord)){
+            alert("valid form")
+        }else{
+            alert("Form has errors")
+        }       
+    }
+   
     render(){
         return(
             <div className = "RegistrationPage">
                  <form>
                     <div className="name">
                         <label>First Name: </label>
-                        <input id="name" type="text" size="30" placeholder="First Name" required="true" />
-                        <p className="error hide name" value="Specify Value in this field"></p>
+                        <input id="name" type="text" size="15" placeholder="First Name" required/> 
                         <br/>
+                        <label>Last Name: </label>
+                            <input id="lastName" type="text" size="15" placeholder="last name" required />
+                            <br/>
+                        <label>Password: </label>
+                            <input id="pass_word" type="password" size="15" placeholder="New password" required/>
+                            <br/>
+                        <label>User Name:</label>
+                            <input id="user" type="text" size="15" placeholder="User Name" required/>
+                            <br/>
                     </div>
                     <button  value="submit" onClick={this.onSubmitClick}>Submit</button>
                 </form>
@@ -114,10 +145,14 @@ export default class Registration extends Component{
 }
 
 /*
-<label>Last Name: </label>
-<input id="lastName" type="text" size="30" placeholder="last name" required="true" />
-<p className="error hide" value="only letters"/>
+<label>First Name: </label>
+<input id="name" type="text" size="10" placeholder="First Name" required/> 
+ <p className="error hide name" value="Specify Value in this field"></p>
 <br/>
+<label>Last Name: </label>
+<input id="lastName" type="text" size="10" placeholder="last name" required="true" />
+<p className="error hide" value="only letters"/>
+
 <label>User Name:</label>
 <input id="userName" type="text" size="30" placeholder="User Name" required="true"/>
 <br/>
